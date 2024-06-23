d9432eafc35c65c8f885642e75306cb960c99ad6
Akalanka Perera <akalankaperera128@gmail.com>
2024-06-23 14:35:51 +0530

    Fix: cybernetic frontier chrome version

--- medium/cybernetic-frontier/execute.sh
+++ medium/cybernetic-frontier/execute.sh
@@ -1,13 +1,13 @@
-# Start Install Google Chrome (You may comment out these lines during local testing if you already have Chrome installed)
+# Start Install Google Chrome (You may comment out these lines during local testing if you already have Chrome installed. Though the chromedriver installed here might not work with your Chrome version)
 
 sudo apt update
 
-wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_122.0.6261.128-1_amd64.deb
-sudo dpkg -i google-chrome-stable_122.0.6261.128-1_amd64.deb
+wget https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_126.0.6478.114-1_amd64.deb
+sudo dpkg -i google-chrome-stable_126.0.6478.114-1_amd64
 
 sudo apt-get install -y -f
 
-rm google-chrome-stable_122.0.6261.128-1_amd64.deb
+rm google-chrome-stable_126.0.6478.114-1_amd64
 
 # End Install Google Chrome
 
